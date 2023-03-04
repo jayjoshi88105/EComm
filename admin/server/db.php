e6104702bf4424b5845bab732eb2c015ddf179b2
Jay Joshi <jayrjoshi510@gmail.com>
2023-03-04 14:47:49 +0530

    Update db.php

--- admin/server/db.php
+++ admin/server/db.php
@@ -5,8 +5,7 @@
 
   try {
     $conn = new PDO("mysql:host=$servername;dbname=ecomm", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //To connect with DB
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
   }
